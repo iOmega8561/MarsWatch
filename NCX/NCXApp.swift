b7cdd4eff28b04bbd5ac9cd95530fea3c79aea56
Giuseppe <giusepperocco38@gmail.com>
2023-03-22 15:19:38 +0100

    Initial Commit

---
+++ NCX/NCXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NCXApp.swift
+//  NCX
+//
+//  Created by Giuseppe Rocco on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NCXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
